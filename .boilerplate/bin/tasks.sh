50af2b70505c43d15bd2739001077f05e503bbde
Rodrigo Passos <rodrigo@hellodative.com>
2023-04-24 13:38:25 -0400

    comment out tear down

--- .boilerplate/bin/tasks.sh
+++ .boilerplate/bin/tasks.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 DDEV_PATH=".ddev"
 CRAFT_PATH="cms"
@@ -38,51 +38,52 @@ ddev_setup() {
             exit 1;
         fi
     fi
-}
-
-cms_setup() {
-    # TODO: let user know that he may need to enter sudo password
-    if [ -d "$CRAFT_PATH" ]; then
-        echo "\033[33mThe cms directory already exists. Skipping CMS setup...\033[0m";
-    else
-        echo "Setting up CMS...";
-        ddev_up;
-        if [ ! -d "$CRAFT_PATH/vendor" ]; then
-            echo "Installing Composer dependencies...";
-            ddev composer create -y --no-scripts craftcms/craft;
-        fi;
-        echo "Copying .boilerplate/cms to $CRAFT_PATH";
-        rsync -ur .boilerplate/cms/. $CRAFT_PATH/;
-        echo "Copying $CRAFT_PATH/example.env to $CRAFT_PATH/.env";
-        cp $CRAFT_PATH/example.env $CRAFT_PATH/.env;
-        echo "Installing Craft...";
-        ddev craft setup/app-id;
-        ddev craft setup/security-key;
-        ADMIN_PASSWORD=$(openssl rand -base64 8);
-        SITE_URL=$(ddev describe -j | jq -r '.raw.primary_url');
-        ddev craft install/craft --interactive=0 --email="$ADMIN_USERNAME" --language="en-US" --password="$ADMIN_PASSWORD" --username="admin" --site-name="$DEFAULT_SITE_NAME" --site-url="$SITE_URL";
-        # Replace PRIMARY_SITE_URL with $DDEV_PRIMARY_URL variable in .env file
-        sed -i '' "s|PRIMARY_SITE_URL=.*|PRIMARY_SITE_URL=\"$\{DDEV_PRIMARY_URL\}\"|g" $CRAFT_PATH/.env;
-        if [ ! -f "$CRAFT_PATH/plugins.txt" ]; then
-            echo "\033[31mCould not find plugins.txt file in $CRAFT_PATH.\033[0m";
-        else
-            echo "Add Craft plugins...";
-            ddev composer config allow-plugins.treeware/plant false;
-            ddev composer require --no-progress --no-scripts --no-interaction --optimize-autoloader --ignore-platform-reqs --update-with-dependencies $(cat $CRAFT_PATH/plugins.txt);
-            ddev craft plugin/install --all;
-        fi; \
-        echo "\n*************************************************************\n";
-        echo "    \033[32mCraft CMS successfully set up!\033[0m\n";
-        echo "    Site URL: $SITE_URL";
-        echo "    Admin URL: $SITE_URL/admin";
-        echo "    Username: $ADMIN_USERNAME";
-        echo "    Password: $ADMIN_PASSWORD\n";
-        echo "    To remove the CMS files and database, run this command:\n";
-        echo "    make cms-teardown";
-        echo "\n*************************************************************\n";
-    fi
     exit 0;
 }
+
+# cms_setup() {
+#     # TODO: let user know that he may need to enter sudo password
+#     if [ -d "$CRAFT_PATH" ]; then
+#         echo "\033[33mThe cms directory already exists. Skipping CMS setup...\033[0m";
+#     else
+#         echo "Setting up CMS...";
+#         ddev_up;
+#         if [ ! -d "$CRAFT_PATH/vendor" ]; then
+#             echo "Installing Composer dependencies...";
+#             ddev composer create -y --no-scripts craftcms/craft;
+#         fi;
+#         echo "Copying .boilerplate/cms to $CRAFT_PATH";
+#         rsync -ur .boilerplate/cms/. $CRAFT_PATH/;
+#         echo "Copying $CRAFT_PATH/example.env to $CRAFT_PATH/.env";
+#         cp $CRAFT_PATH/example.env $CRAFT_PATH/.env;
+#         echo "Installing Craft...";
+#         ddev craft setup/app-id;
+#         ddev craft setup/security-key;
+#         ADMIN_PASSWORD=$(openssl rand -base64 8);
+#         SITE_URL=$(ddev describe -j | jq -r '.raw.primary_url');
+#         ddev craft install/craft --interactive=0 --email="$ADMIN_USERNAME" --language="en-US" --password="$ADMIN_PASSWORD" --username="admin" --site-name="$DEFAULT_SITE_NAME" --site-url="$SITE_URL";
+#         # Replace PRIMARY_SITE_URL with $DDEV_PRIMARY_URL variable in .env file
+#         sed -i '' "s|PRIMARY_SITE_URL=.*|PRIMARY_SITE_URL=\"$\{DDEV_PRIMARY_URL\}\"|g" $CRAFT_PATH/.env;
+#         if [ ! -f "$CRAFT_PATH/plugins.txt" ]; then
+#             echo "\033[31mCould not find plugins.txt file in $CRAFT_PATH.\033[0m";
+#         else
+#             echo "Add Craft plugins...";
+#             ddev composer config allow-plugins.treeware/plant false;
+#             ddev composer require --no-progress --no-scripts --no-interaction --optimize-autoloader --ignore-platform-reqs --update-with-dependencies $(cat $CRAFT_PATH/plugins.txt);
+#             ddev craft plugin/install --all;
+#         fi; \
+#         echo "\n*************************************************************\n";
+#         echo "    \033[32mCraft CMS successfully set up!\033[0m\n";
+#         echo "    Site URL: $SITE_URL";
+#         echo "    Admin URL: $SITE_URL/admin";
+#         echo "    Username: $ADMIN_USERNAME";
+#         echo "    Password: $ADMIN_PASSWORD\n";
+#         echo "    To remove the CMS files and database, run this command:\n";
+#         echo "    make cms-teardown";
+#         echo "\n*************************************************************\n";
+#     fi
+#     exit 0;
+# }
 
 cms_teardown() {
     # Prompt user to confirm that he wants to delete the CMS
