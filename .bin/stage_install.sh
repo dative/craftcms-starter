54c1a263b67c6760e502f9777a6695cd5e5b33a2
Rodrigo Passos <rodrigo@hellodative.com>
2023-05-02 13:00:57 -0400

    make stage_install.sh testable

--- .bin/stage_install.sh
+++ .bin/stage_install.sh
@@ -4,19 +4,36 @@
 DIR="$(dirname "${BASH_SOURCE[0]}")"
 source "${DIR}/common.sh"
 
-# check if PROJECT_TEST_NAME exists, if not create it
-if [ -d "$PROJECT_TEST_NAME" ]; then
-    make_output "    \033[32mLocal test setup already exists!\033[0m\n";
-else
-    # mkdir $PROJECT_TEST_NAME
-    make_output "    \033[32mLocal test setup completed!\033[0m\n";
+stage_test() {
+    # check if PROJECT_TEST_NAME exists, if not create it
+    if [ -d "$PROJECT_TEST_NAME" ]; then
+        make_output "    \033[32mLocal test setup already exists!\033[0m\n";
+        exit 0;
+    else
+        mkdir $PROJECT_TEST_NAME
+        make_output "    \033[32mLocal test setup completed!\033[0m\n";
+    fi
+    
+    # # Copy the Makefile and the .bin directory to PROJECT_TEST_NAME
+    cp Makefile $PROJECT_TEST_NAME
+    cp -r .bin $PROJECT_TEST_NAME
+    
+    make_output "    To run the install, run the following:\n";
+    make_output "    cd $PROJECT_TEST_NAME && make setup-project";
+    
+    print_output
+}
+
+run_main() {
+    stage_test || exit 1
+}
+
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
+then
+    run_main
+    if [ $? -gt 0 ]
+    then
+        exit 1
+    fi
 fi
-
-# # Copy the Makefile and the .bin directory to PROJECT_TEST_NAME
-# cp Makefile $PROJECT_TEST_NAME
-# cp -r .bin $PROJECT_TEST_NAME
-
-make_output "    To run the install, run the following:\n";
-make_output "    cd $PROJECT_TEST_NAME && make setup-project";
-
-print_output
