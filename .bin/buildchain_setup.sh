b904db1c4e761b13287421fc70ba5088bb663fa3
Rodrigo Passos <rodrigo@hellodative.com>
2023-05-04 00:48:51 -0400

    minor tweaks

--- .bin/buildchain_setup.sh
+++ .bin/buildchain_setup.sh
@@ -4,16 +4,6 @@
 DIR="$(dirname "${BASH_SOURCE[0]}")"
 source "${DIR}/common.sh"
 
-copy_config_files() {
-  mkdir "$DIR"/tmp
-  cp "$DIR"/buildchain/template.* "$DIR"/tmp
-  cd "$DIR"/tmp || exit
-  for file in template.*; do mv "$file" "${file#template.}"; done
-  cd "$PROJECT_ROOT" || exit
-  rsync -ur "$DIR"/tmp/. "$PROJECT_ROOT"
-  rm -rf "$DIR"/tmp
-}
-
 copy_src() {
   local INSTALL_SRC_PATH
   local FULL_INSTALL_SRC_PATH
@@ -52,19 +42,30 @@ copy_github() {
 
 copy_root_files() {
   local INSTALL_ROOT_FILES_PATH
-  local FULL_INSTALL_ROOT_FILES_PATH
 
   INSTALL_ROOT_FILES_PATH=$(realpath -m --relative-base="$(pwd)" "$BASE_PATH/$PROJECT_ROOT")
-  FULL_INSTALL_ROOT_FILES_PATH=$(realpath -m "$INSTALL_ROOT_FILES_PATH")
-
+  raise "$INSTALL_ROOT_FILES_PATH\n"
   raise "Copying $DIR/buildchain/template.* to $INSTALL_ROOT_FILES_PATH\n"
   mkdir -p "$DIR/tmp"
   cp "$DIR"/buildchain/template.* "$DIR"/tmp
   cd "$DIR/tmp" || exit 1
   for file in template.*; do mv "$file" "${file#template.}"; done
   cd "$PROJECT_ROOT" || exit 1
-  rsync -ur "$DIR/tmp/." "$FULL_INSTALL_ROOT_FILES_PATH/"
+  rsync -ur "$DIR/tmp/." "$PROJECT_ROOT/"
   rm -rf "$DIR"/tmp
+}
+
+update_root_files() {
+  local PROJECT_NAME
+  PROJECT_NAME=$(ddev describe -j | jq -r '.raw.name')
+  PROJECT_URL=$(ddev describe -j | jq -r '.raw.hostname')
+  if [ -f "package.json" ]; then
+    sed -i '' -e "s/craftcms-starter/$PROJECT_NAME-craftcms/g" package.json || return 1
+  fi
+  if [ -f "README.md" ]; then
+    sed -i '' -e "s/###PROJECT_NAME###/$PROJECT_NAME-craftcms/g" README.md || return 1
+    sed -i '' -e "s/###PROJECT_URL###/$PROJECT_URL/g" README.md || return 1
+  fi
 }
 
 setup_buildchain() {
@@ -76,12 +77,7 @@ setup_buildchain() {
 
   if [ -d "$DDEV_PATH" ]; then
     cd "$BASE_PATH" || return 1
-    PROJECT_NAME=$(ddev describe -j | jq -r '.raw.name')
-
-    # TODO: Update README.md, package.json, vite, etc. with project name
-    # sed -i "s/###PROJECT_NAME###/$PROJECT_NAME/gi" $DIR/README.md
-
-    add_to_summary "Project Name: $PROJECT_NAME\n"
+    update_root_files || return 1
     add_to_summary "To start development run this command:\n"
     add_to_summary "ddev yarn && ddev yarn start"
   fi
@@ -89,7 +85,6 @@ setup_buildchain() {
   print_summary "success"
 
   return 0
-
 }
 
 run_main() {
