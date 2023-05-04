2b82446a87a6758e5b8a48cb341e27ca918fa664
Rodrigo Passos <rodrigo@hellodative.com>
2023-05-04 00:06:34 -0400

    minor refactor on ddev_setup

--- .bin/ddev_setup.sh
+++ .bin/ddev_setup.sh
@@ -33,13 +33,14 @@ get_ddev_config_options() {
 
 setup_ddev() {
 
-  local INSTALL_PATH
+  local INSTALL_DDEV_PATH
+  local FULL_INSTALL_DDEV_PATH
 
-  INSTALL_PATH=$(realpath -m --relative-base="$(pwd)" "$BASE_PATH/$DDEV_PATH")
-  FULL_INSTALL_PATH=$(realpath -m "$INSTALL_PATH")
+  INSTALL_DDEV_PATH=$(realpath -m --relative-base="$(pwd)" "$BASE_PATH/$DDEV_PATH")
+  FULL_INSTALL_DDEV_PATH=$(realpath -m "$INSTALL_DDEV_PATH")
 
-  if [ -d "$FULL_INSTALL_PATH" ]; then
-    raise "The $INSTALL_PATH directory already exists. Skipping DDEV setup...\n" "warning"
+  if [ -d "$FULL_INSTALL_DDEV_PATH" ]; then
+    raise "The $INSTALL_DDEV_PATH directory already exists. Skipping DDEV setup...\n" "warning"
     return 0
   fi
 
@@ -50,12 +51,12 @@ setup_ddev() {
   # Copy .bin/ddev to .ddev
   raise "Copying $DIR/ddev to $DDEV_PATH\n"
 
-  mkdir -p "$INSTALL_PATH"
-  rsync -ur "$DIR"/ddev/. "$INSTALL_PATH"/
+  mkdir -p "$INSTALL_DDEV_PATH"
+  rsync -ur "$DIR"/ddev/. "$INSTALL_DDEV_PATH"/
 
   # Rename sample.config.m1.yaml to config.m1.yaml if Apple Silicon
   if [ "$(uname -m)" = "arm64" ]; then
-    cp "$INSTALL_PATH"/sample.config.m1.yaml "$INSTALL_PATH"/config.m1.yaml
+    cp "$INSTALL_DDEV_PATH"/sample.config.m1.yaml "$INSTALL_DDEV_PATH"/config.m1.yaml
   fi
 
   cd "$BASE_PATH" || return 1
@@ -67,20 +68,20 @@ setup_ddev() {
     add_to_summary "To start DDEV, run this command:\n"
     add_to_summary "ddev start\n\n"
     add_to_summary "To remove and unlist this project, run this command:\n"
-    add_to_summary "ddev stop --unlist $PROJECT_NAME && rm -rf $INSTALL_PATH"
+    add_to_summary "ddev stop --unlist $PROJECT_NAME && rm -rf $INSTALL_DDEV_PATH"
     print_summary "success"
   else
     add_to_summary "Can't create the project due to the following error:\n"
     add_to_summary "$error_msg\n"
     add_to_summary "Cleaning up and exiting..."
     print_summary "danger"
-    rm -rf "$FULL_INSTALL_PATH"
+    rm -rf "$FULL_INSTALL_DDEV_PATH"
     exit 1
   fi
 }
 
 run_main() {
-  check_required_environment "DDEV_PATH" || return 1
+  check_required_environment "BASE_PATH DDEV_PATH" || return 1
   check_required_ddev_command || return 1
   check_if_args_unique "$@" || return 1
   setup_ddev "$@"
